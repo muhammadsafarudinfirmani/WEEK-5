0a7c34b06829d5f8c2157df0485640496754644a
muhammadsafarudinfirmani <muhammadsafarudinfirmani@gmail.com>
2025-10-29 13:58:28 +0700

    first commit

---
+++ segitiga.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class segitiga {
+
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        double alas ;
+        double tinggi;
+        double a;
+        double b;
+        double c;
+        double luas;
+        double keliling;
+
+        System.out.println("alas :");
+        alas = myObj.nextDouble();
+        System.out.println("tinggi :");
+        tinggi = myObj.nextDouble();
+        System.out.println("a :");
+        a = myObj.nextDouble();
+        System.out.println("b :");
+        b = myObj.nextDouble();
+        System.out.println("c :");
+        c = myObj.nextDouble();
+
+        luas = 0.5 * alas * tinggi;  
+        
+        keliling = a + b + c;
+
+        System.out.println("hasil luas :" + luas);
+        System.out.println("keliling :2" + keliling);
+    }
+}
